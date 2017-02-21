98736f206c2d0c22b35c516a21ef1a197e6f0da6
astepaniuk <astepaniuk@homeaway.com>
2017-02-21 09:30:43 +0200

    Added Refenue - Expenditure feature

--- college-shutdowns.R
+++ college-shutdowns.R
@@ -18,9 +18,9 @@ compare_roc_curves <- function(frame, title){
   frame.factor <- frame
   frame.factor$NON_OPERATING <- as.factor(frame$NON_OPERATING)
   nb.model <- suppressWarnings(train(NON_OPERATING~., subset=train_ind, data=frame.factor, 'nb', trControl=trainControl(method='cv', number=10)))
-  nb.pred <- suppressWarnings(predict(nb.model$finalModel, newdata=frame.factor[test_ind,], ))
+  nb.pred <- suppressWarnings(predict(nb.model$finalModel, newdata=frame.factor[test_ind,]))
   roc.nb <- simple_roc(frame[test_ind,"NON_OPERATING"], unname(nb.pred$posterior[,2]))
-  auc.nb <- auc(x = roc.nb$FPR, y = roc.nb$TPR) # TODO why roc.nb$FPR are NaNs ?
+  auc.nb <- auc(x = roc.nb$FPR, y = roc.nb$TPR)
 
   # LDA
   lda.fit <- suppressWarnings(lda(NON_OPERATING ~ ., data=frame, subset = train_ind))
@@ -85,32 +85,32 @@ file.2012 <- "./data/MERGED2012_PP.csv"
 #raw.2012 <- read.csv(file.2012, stringsAsFactors = FALSE)
 
 # Feature set #1 (Program percentages)
-fs1.2013 <- c("PCIP01", "PCIP03", "PCIP04", "PCIP05", "PCIP09", "PCIP10", "PCIP11", "PCIP12", "PCIP13", "PCIP14", "PCIP15", "PCIP16", "PCIP19", "PCIP22", "PCIP23", "PCIP24", "PCIP25", "PCIP26", "PCIP27", "PCIP29", "PCIP30", "PCIP31", "PCIP38", "PCIP39", "PCIP40", "PCIP41", "PCIP42", "PCIP43", "PCIP44", "PCIP45", "PCIP46", "PCIP47", "PCIP48", "PCIP49", "PCIP50", "PCIP51", "PCIP52", "PCIP54")
-frame1.2013 <- raw.2013[,fs1.2013]
+features1.2013 <- c("PCIP01", "PCIP03", "PCIP04", "PCIP05", "PCIP09", "PCIP10", "PCIP11", "PCIP12", "PCIP13", "PCIP14", "PCIP15", "PCIP16", "PCIP19", "PCIP22", "PCIP23", "PCIP24", "PCIP25", "PCIP26", "PCIP27", "PCIP29", "PCIP30", "PCIP31", "PCIP38", "PCIP39", "PCIP40", "PCIP41", "PCIP42", "PCIP43", "PCIP44", "PCIP45", "PCIP46", "PCIP47", "PCIP48", "PCIP49", "PCIP50", "PCIP51", "PCIP52", "PCIP54")
+frame1.2013 <- raw.2013[,features1.2013]
 frame1.2013$NON_OPERATING <- ifelse(raw.2013[,"CURROPER"]==0, 1, 0)
-frame1.2013[,fs1.2013] <- suppressWarnings(lapply(frame1.2013[,fs1.2013], as.numeric))
+frame1.2013[,features1.2013] <- suppressWarnings(lapply(frame1.2013[,features1.2013], as.numeric))
 frame1.2013 <- frame1.2013[complete.cases(frame1.2013),]
 frame1.2013 <- get.balanced.frame(frame1.2013)
 
 # Feature set #2 (EXPLORE THIS - what feature have the biggest impact?)
-fs2.2013 <- c("DISTANCEONLY", "TUITIONFEE_IN", "TUITIONFEE_OUT", "TUITFTE", "INEXPFTE", "AVGFACSAL", "PFTFAC", "PAR_ED_PCT_MS", "PAR_ED_PCT_HS", "PAR_ED_PCT_PS")
+features2.2013 <- c("DISTANCEONLY", "TUITIONFEE_IN", "TUITIONFEE_OUT", "TUITFTE", "INEXPFTE", "AVGFACSAL", "PFTFAC", "PAR_ED_PCT_MS", "PAR_ED_PCT_HS", "PAR_ED_PCT_PS")
 # TODO remove - for debug only
-#frame2.2013 <- raw.2013[,c("ï.¿UNITID", "INSTNM", fs2.2013)]
+#frame2.2013 <- raw.2013[,c("ï.¿UNITID", "INSTNM", features2.2013)]
 #colnames(frame2.2013)[1] <- "ID"
-frame2.2013 <- raw.2013[,fs2.2013]
+frame2.2013 <- raw.2013[,features2.2013]
 frame2.2013$NON_OPERATING <- ifelse(raw.2013[,"CURROPER"]==0, 1, 0)
-frame2.2013[,fs2.2013] <- suppressWarnings(lapply(frame2.2013[,fs2.2013], as.numeric))
+frame2.2013[,features2.2013] <- suppressWarnings(lapply(frame2.2013[,features2.2013], as.numeric))
 frame2.2013 <- frame2.2013[complete.cases(frame2.2013),]
 frame2.2013 <- get.balanced.frame(frame2.2013)
 
 # Feature set #3 (Admission + Undergrads)
-fs3.2013 <- c("ADM_RATE_ALL", "UGDS")
+features3.2013 <- c("ADM_RATE_ALL", "UGDS")
 # TODO remove - for debug only
-#frame3.2013 <- raw.2013[,c("ï.¿UNITID", "INSTNM", fs3.2013)]
+#frame3.2013 <- raw.2013[,c("ï.¿UNITID", "INSTNM", features3.2013)]
 #colnames(frame3.2013)[1] <- "ID"
-frame3.2013 <- raw.2013[,fs3.2013]
+frame3.2013 <- raw.2013[,features3.2013]
 frame3.2013$NON_OPERATING <- ifelse(raw.2013[,"CURROPER"]==0, 1, 0)
-frame3.2013[,fs3.2013] <- suppressWarnings(lapply(frame3.2013[,fs3.2013], as.numeric))
+frame3.2013[,features3.2013] <- suppressWarnings(lapply(frame3.2013[,features3.2013], as.numeric))
 frame3.1.2013 <- frame3.2013[complete.cases(frame3.2013),]
 is.adm.rate.not.null <- complete.cases(frame3.2013$ADM_RATE_ALL)
 average_adm_rate <- mean(frame3.2013$ADM_RATE_ALL[is.adm.rate.not.null])
@@ -119,13 +119,13 @@ frame3.2.2013 <- frame3.2013[complete.cases(frame3.2013),]
 frame3.2013 <- get.balanced.frame(frame3.2013)
 
 # Feature set #4 (Cost)
-fs4.2013 <- c("TUITIONFEE_PROG", "TUITIONFEE_OUT", "COSTT4_A", "NPT4_PUB", "NPT4_PRIV", "NPT4_PROG", "NPT4_OTHER", "NPT41_PUB", "NPT42_PUB", "NPT43_PUB", "NPT44_PUB", "NPT45_PUB", "NPT41_PRIV", "NPT42_PRIV", "NPT43_PRIV", "NPT44_PRIV", "NPT45_PRIV", "NPT41_PROG", "NPT42_PROG", "NPT43_PROG", "NPT44_PROG", "NPT45_PROG", "NPT41_OTHER", "NPT42_OTHER", "NPT43_OTHER", "NPT44_OTHER", "NPT45_OTHER", "NUM4_PUB", "NUM4_PRIV", "NUM4_PROG", "NUM4_OTHER", "NUM41_PUB", "NUM42_PUB", "NUM43_PUB", "NUM44_PUB", "NUM45_PUB", "NUM41_PRIV", "NUM42_PRIV", "NUM43_PRIV", "NUM44_PRIV", "NUM45_PRIV", "NUM41_PROG", "NUM42_PROG", "NUM43_PROG", "NUM44_PROG", "NUM45_PROG", "NUM41_OTHER", "NUM42_OTHER", "NUM43_OTHER", "NUM44_OTHER", "NUM45_OTHER")
+features4.2013 <- c("TUITIONFEE_PROG", "TUITIONFEE_OUT", "COSTT4_A", "NPT4_PUB", "NPT4_PRIV", "NPT4_PROG", "NPT4_OTHER", "NPT41_PUB", "NPT42_PUB", "NPT43_PUB", "NPT44_PUB", "NPT45_PUB", "NPT41_PRIV", "NPT42_PRIV", "NPT43_PRIV", "NPT44_PRIV", "NPT45_PRIV", "NPT41_PROG", "NPT42_PROG", "NPT43_PROG", "NPT44_PROG", "NPT45_PROG", "NPT41_OTHER", "NPT42_OTHER", "NPT43_OTHER", "NPT44_OTHER", "NPT45_OTHER", "NUM4_PUB", "NUM4_PRIV", "NUM4_PROG", "NUM4_OTHER", "NUM41_PUB", "NUM42_PUB", "NUM43_PUB", "NUM44_PUB", "NUM45_PUB", "NUM41_PRIV", "NUM42_PRIV", "NUM43_PRIV", "NUM44_PRIV", "NUM45_PRIV", "NUM41_PROG", "NUM42_PROG", "NUM43_PROG", "NUM44_PROG", "NUM45_PROG", "NUM41_OTHER", "NUM42_OTHER", "NUM43_OTHER", "NUM44_OTHER", "NUM45_OTHER")
 # TODO remove - for debug only
-#frame4.2013 <- raw.2013[,c("ï.¿UNITID", "INSTNM", fs4.2013)]
+#frame4.2013 <- raw.2013[,c("ï.¿UNITID", "INSTNM", features4.2013)]
 #colnames(frame4.2013)[1] <- "ID"
-frame4.2013 <- raw.2013[,fs4.2013]
+frame4.2013 <- raw.2013[,features4.2013]
 frame4.2013$NON_OPERATING <- ifelse(raw.2013[,"CURROPER"]==0, 1, 0)
-frame4.2013[,fs4.2013] <- suppressWarnings(lapply(frame4.2013[,fs4.2013], as.numeric))
+frame4.2013[,features4.2013] <- suppressWarnings(lapply(frame4.2013[,features4.2013], as.numeric))
 # merging related columns
 frame4.2013 <- merge_columns(frame4.2013, c("TUITIONFEE_PROG", "TUITIONFEE_OUT"), "TUITIONFEE")
 frame4.2013 <- merge_columns(frame4.2013, c( "NPT4_PUB", "NPT4_PRIV", "NPT4_PROG", "NPT4_OTHER"), "NPT4")
@@ -141,42 +141,31 @@ frame4.2013 <- merge_columns(frame4.2013, c( "NUM43_PUB", "NUM43_PRIV", "NUM43_P
 frame4.2013 <- merge_columns(frame4.2013, c( "NUM44_PUB", "NUM44_PRIV", "NUM44_PROG", "NUM44_OTHER"), "NUM44")
 frame4.2013 <- merge_columns(frame4.2013, c( "NUM45_PUB", "NUM45_PRIV", "NUM45_PROG", "NUM45_OTHER"), "NUM45")
 
-# TODO find out, how many null are in every column
-sum(is.na(frame4.2013$NPT4))
-sum(is.na(frame4.2013$NPT41))
-sum(is.na(frame4.2013$NPT42))
-sum(is.na(frame4.2013$NPT43))
-sum(is.na(frame4.2013$NPT44)) #3530
-sum(is.na(frame4.2013$NPT45)) #4320
-sum(is.na(frame4.2013$NUM4))
-sum(is.na(frame4.2013$NUM41))
-sum(is.na(frame4.2013$NUM42))
-sum(is.na(frame4.2013$NUM43))
-sum(is.na(frame4.2013$NUM44)) #3530
-sum(is.na(frame4.2013$NUM45)) #4320
-
-frame4.1.2013 <- frame4.2013[,c("NON_OPERATING", "NPT4", "NPT41", "NPT42", "NPT43", "NUM4", "NUM41", "NUM42", "NUM43")]
-frame4.1.2013 <- frame4.1.2013[complete.cases(frame4.1.2013),]
-frame4.1.2013 <- get.balanced.frame(frame4.1.2013)
-
+# excluding 44 and 45 because they have to many NAs
+frame4.2013 <- frame4.2013[,c("NON_OPERATING", "NPT4", "NPT41", "NPT42", "NPT43", "NUM4", "NUM41", "NUM42", "NUM43")]
 frame4.2013 <- frame4.2013[complete.cases(frame4.2013),]
-frame4.2013.unbalanced <- frame4.2013
 frame4.2013 <- get.balanced.frame(frame4.2013)
 
-# TODO Why so many NULLS? (7804 - 2654) ?
+# Feature set #5 (Revenue - Expenditure per student)
+features5.2013 <- c("TUITFTE", "INEXPFTE")
+frame5.2013 <- raw.2013[,features5.2013]
+frame5.2013$NON_OPERATING <- ifelse(raw.2013[,"CURROPER"]==0, 1, 0)
+frame5.2013[,features5.2013] <- suppressWarnings(lapply(frame5.2013[,features5.2013], as.numeric))
+frame5.2013 <- frame5.2013[complete.cases(frame5.2013),]
+frame5.2013$REVENUE_EXPENDITURE <- frame5.2013$TUITFTE - frame5.2013$INEXPFTE
+frame5.2013$TUITFTE <- NULL
+frame5.2013$INEXPFTE <- NULL
+frame5.2013 <- get.balanced.frame(frame5.2013)
 
-
-# Feature set #5 (School)
-#features.2013 <- c("TUITFTE", "INEXPFTE", "AVGFACSAL", "PFTFAC")
-# Feature set #5 (Completion/Retention)
+# Feature set #6 (Completion/Retention)
 #features.2013 <- c("RET_PTL4", "RET_PT4", "RET_FTL4", "RET_FT4", "C150_4", "C150_L4", "C150_4_POOLED", "C150_L4_POOLED", "D150_4", "D150_L4", "D150_4_POOLED", "D150_L4_POOLED", "C150_4_AIAN", "C150_4_NHPI", "C150_4_2MOR", "C150_4_NRA", "C150_4_API", "C150_L4_NHPI", "C150_L4_2MOR", "C150_L4_NRA", "C150_L4_POOLED_SUP", "C150_4_POOLED_SUPP", "C200_L4_POOLED_SUP", "C200_4_POOLED_SUPP")
 
-compare_roc_curves(frame1.2013, "FS#1 (Program percentages. Imbalanced)")
-compare_roc_curves(frame2.2013, "FS#2 (NEEDS ANALYSIS. Imbalanced)")
-compare_roc_curves(frame3.1.2013, "FS#3 (Admission Rate(Ignoring NA) + Undergrads. Imbalanced)")
-compare_roc_curves(frame3.2.2013, "FS#3 (Admission Rate(NA=ave.) + Undergrads. Imbalanced)")
-compare_roc_curves(frame4.2013, "FS#4 (Cost. >5000 NAs. Imbalanced")
-compare_roc_curves(frame4.1.2013, "FS#4.1 (Cost. 5200 entries.")
+compare_roc_curves(frame1.2013, "FS#1 (Program percentages.)")
+compare_roc_curves(frame2.2013, "FS#2 (NEEDS ANALYSIS.)")
+compare_roc_curves(frame3.1.2013, "FS#3 (Admission Rate(Ignoring NA) + Undergrads.)")
+compare_roc_curves(frame3.2.2013, "FS#3 (Admission Rate(NA=ave.) + Undergrads.)")
+compare_roc_curves(frame4.2013, "FS#4 (Cost)")
+compare_roc_curves(frame5.2013, "FS#5 (Revenue - expenditure)")
 
 
 
